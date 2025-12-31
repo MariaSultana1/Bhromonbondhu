4cddbc83ab425334a72463f85d7e85e1c5294269
Maria <m99017136@gmail.com>
2025-12-31 11:33:09 +0600

    LoginPage_Update

--- sm-auth/src/App.jsx
+++ sm-auth/src/App.jsx
@@ -30,10 +30,11 @@
 
 
 import { useState, useEffect } from 'react';
-import { AuthPage } from './components/AuthPage';
+//import { AuthPage } from './components/AuthPage';
 import { TravelerDashboard } from './components/TravelerDashboard';
 import { HostDashboard } from './components/HostDashboard';
 import { AdminDashboard } from './components/AdminDashboard';
+import {Auth} from './components/Auth';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -62,7 +63,7 @@ function App() {
   };
 
   if (!isAuthenticated || !currentUser) {
-    return <AuthPage onLogin={handleLogin} />;
+    return <Auth onLogin={handleLogin} />;
   }
 
   return (
